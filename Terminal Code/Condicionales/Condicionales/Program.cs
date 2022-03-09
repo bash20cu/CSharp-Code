aeaf13dfc70a43a6840d2a24fa126dd114c71a07
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-03-09 15:02:39 -0600

    namespaces call
    namespace call

--- Terminal Code/Condicionales/Condicionales/Program.cs
+++ Terminal Code/Condicionales/Condicionales/Program.cs
@@ -9,30 +9,19 @@
 //Random coin = new Random();
 //Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
 
-string permission = "Admin|Manager";
-int level = 10;
+//string permission = "Admin|Manager";
+//int level = 10;
 
-if (permission.Contains("Admin") | permission.Contains("Manager") )
-{
-    if (level > 55)
-    {
-        Console.WriteLine("Welcome, Super Admin user.");
-    }
-    else
-    {
-        Console.WriteLine("Welcome, Admin user.");
-    }
-    if (level >= 20)
-    {
-        Console.WriteLine("Contact an Admin for access.");
-    }
-    else
-    {
-        Console.WriteLine("You do not have sufficient privileges.");
-    }
-}
-//else if (permission.Contains("Manager"))
+//if (permission.Contains("Admin") | permission.Contains("Manager") )
 //{
+//    if (level > 55)
+//    {
+//        Console.WriteLine("Welcome, Super Admin user.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Welcome, Admin user.");
+//    }
 //    if (level >= 20)
 //    {
 //        Console.WriteLine("Contact an Admin for access.");
@@ -42,7 +31,19 @@ if (permission.Contains("Admin") | permission.Contains("Manager") )
 //        Console.WriteLine("You do not have sufficient privileges.");
 //    }
 //}
-else
-{
-    Console.WriteLine("You do not have sufficient privileges.");
-}
+////else if (permission.Contains("Manager"))
+////{
+////    if (level >= 20)
+////    {
+////        Console.WriteLine("Contact an Admin for access.");
+////    }
+////    else
+////    {
+////        Console.WriteLine("You do not have sufficient privileges.");
+////    }
+////}
+//else
+//{
+//    Console.WriteLine("You do not have sufficient privileges.");
+//}
+
---
+++ Terminal Code/TerminalApps/LlamadasVariables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Microsoft Learn";
+            string reversedValue = Utilities.Utility.Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+        }
+        
+        
+        
+        //static string Reverse(string message)
+        //{
+        //    char[] letters = message.ToCharArray();
+        //    Array.Reverse(letters);
+        //    return new string(letters);
+        //}
+    }    
+}
+
+namespace MyNewApp.Utilities
+{
+    class Utility
+    {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
