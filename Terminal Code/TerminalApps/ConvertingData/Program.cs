68c35a01e75a9a0c3d78295bda8dc45f84b027d4
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-03-10 13:20:24 -0600

    Terminal Apps
    Loop and converting data on terminal apps

---
+++ Terminal Code/TerminalApps/ConvertingData/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ConvertingData
+{
+    class MyClass
+    {
+        public static void Main( string[] args)
+        {
+            string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+            decimal result = 0;
+            string message = "";
+            decimal total = 0;
+
+            Console.WriteLine("Loop for converting data from a array: ");
+
+            for (int i = 0; i < values.Length; ++i)
+            {
+
+                Console.WriteLine($"Array content in index : {i} : Value: {values[i]} ");
+
+                if (decimal.TryParse(values[i], out result))
+                {
+
+                    total = total + result;
+                }
+                else
+                {
+                    message = message + values[i];
+                }
+            }
+            
+            Console.WriteLine($"Message string: {message}, Total integer: {total}");
+
+            int value1 = 12;
+            decimal value2 = 6.2m;
+            float value3 = 4.3f;
+
+            // Your code here to set result1
+            int result1 = value1 / (int)value2;
+            Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+            // Your code here to set result2
+            decimal result2 = value2 / (decimal)value3;
+            Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+            // Your code here to set result3
+            float result3 = value3 / (float)value1;
+            Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+        }
+    }
+}
--- Terminal Code/TerminalApps/LlamadasVariables/Program.cs
+++ Terminal Code/TerminalApps/LlamadasVariables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyNewApp.Utilities;
 
 namespace MyNewApp
 {
@@ -6,19 +7,29 @@ namespace MyNewApp
     {
         static void Main(string[] args)
         {
-            string value = "Microsoft Learn";
-            string reversedValue = Utilities.Utility.Reverse(value);
+            string value = "Miguel Alejandro";
+            string reversedValue = Utility.Reverse(value);
             Console.WriteLine($"Secret message: {reversedValue}");
+
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+            bool found = false;
+            int total = 0;
+            foreach (int number in numbers)
+            {                
+                total += number;
+                if (number == 42)
+                {
+                    found = true;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine("Set contains 42");
+            }
+            Console.WriteLine($"Total: {total}");
+
         }
         
-        
-        
-        //static string Reverse(string message)
-        //{
-        //    char[] letters = message.ToCharArray();
-        //    Array.Reverse(letters);
-        //    return new string(letters);
-        //}
     }    
 }
 
@@ -34,3 +45,4 @@ namespace MyNewApp.Utilities
         }
     }
 }
+
