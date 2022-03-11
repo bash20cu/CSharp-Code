7a4ca6d4914e5db56dd30b4416f2f4477863a228
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-03-11 11:16:30 -0600

    Arrays

--- Terminal Code/TerminalApps/TerminalApps/ArraysExample.cs
+++ Terminal Code/TerminalApps/TerminalApps/ArraysExample.cs
@@ -12,6 +12,15 @@ namespace TerminalApps
         {
             string[] pallets = { "B14", "A11", "B12", "A13" };
 
+            // Show the original Array.
+            Console.WriteLine("Original Array: ... "); 
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+            Console.WriteLine(" ");
+
+            // Sort the Array
             Console.WriteLine("Sorted...");
             Array.Sort(pallets);
             foreach (var pallet in pallets)
@@ -19,9 +28,17 @@ namespace TerminalApps
                 Console.WriteLine($"-- {pallet}");
             }
 
+            // Reversed Sorted Array
             Console.WriteLine("");
             Console.WriteLine("Reversed...");
             Array.Reverse(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Array.Clear(pallets, 0, 2);
+            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
             foreach (var pallet in pallets)
             {
                 Console.WriteLine($"-- {pallet}");
