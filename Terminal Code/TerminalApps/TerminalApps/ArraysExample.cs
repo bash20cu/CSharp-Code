ccfe67e107eb006acff65440f4266a9bc6f778a3
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-03-15 11:51:24 -0600

    Array.Resize

--- Terminal Code/TerminalApps/TerminalApps/ArraysExample.cs
+++ Terminal Code/TerminalApps/TerminalApps/ArraysExample.cs
@@ -37,8 +37,33 @@ namespace TerminalApps
                 Console.WriteLine($"-- {pallet}");
             }
 
+            // Clear 2 records
+            Console.WriteLine("");
             Array.Clear(pallets, 0, 2);
             Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+
+            
+            Console.WriteLine("");
+            Array.Resize(ref pallets, 6);
+            Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+            pallets[4] = "C01";
+            pallets[5] = "C02";
+
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            Console.WriteLine("");
+            Array.Resize(ref pallets, 3);
+            Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
             foreach (var pallet in pallets)
             {
                 Console.WriteLine($"-- {pallet}");
