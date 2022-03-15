a62a7ef9ea729f3de3759071c8c72898488beb03
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-03-15 12:31:02 -0600

    string.join

--- Terminal Code/TerminalApps/TerminalApps/ArraysExample.cs
+++ Terminal Code/TerminalApps/TerminalApps/ArraysExample.cs
@@ -69,6 +69,19 @@ namespace TerminalApps
                 Console.WriteLine($"-- {pallet}");
             }
 
+            Console.WriteLine("");
+            string value = "abc123";
+            char[] valueArray = value.ToCharArray();
+            Array.Reverse(valueArray);
+            // string result = new string(valueArray);
+            string result = String.Join(",", valueArray);
+            Console.WriteLine(result);
+
+            string[] items = result.Split(',');
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
