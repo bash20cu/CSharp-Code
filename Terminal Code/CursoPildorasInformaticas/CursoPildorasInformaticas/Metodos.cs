2b06d5f99a008e44cf8ea888df822d6bf8b5ec4d
miguel alejandro fernandez arteaga <53372986+bash20cu@users.noreply.github.com>
2022-04-02 11:51:04 -0600

    Creacion de Metodos

---
+++ Terminal Code/CursoPildorasInformaticas/CursoPildorasInformaticas/Metodos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPildorasInformaticas
+{
+    internal class Metodos
+    {
+        // Metodo que no devuelve nada.
+        public static void sumaNumeros()
+        {
+            int numero1 = 7, numero2 = 9;
+            int resultado = numero1 + numero2;
+            Console.WriteLine(resultado);
+        }
+        //Metodo que devuelve un resultado, pero no tiene parametros
+        public static int sumaNumerosReslt()
+        {
+            int numero1 = 7, numero2 = 9;
+            int resultado = numero1 + numero2;
+            return resultado;
+        } 
+
+        //Metodo que recibe 2 parametros y devuelve un resultado
+        public static int sumaNumerosConParam(int numero1, int numero2)
+        {
+            int resultado = numero1 + numero2;
+            return resultado;
+        }
+
+        // Metodo simple 
+        public static int divideNumeros(int numero1, int numero2) => numero1 / numero2;
+
+        //Ambito y alcanze 
+        
+
+
+    }
+}
--- Terminal Code/CursoPildorasInformaticas/CursoPildorasInformaticas/Program.cs
+++ Terminal Code/CursoPildorasInformaticas/CursoPildorasInformaticas/Program.cs
@@ -6,7 +6,7 @@ namespace CursoPildorasInformaticas
     {
         public static void Main(string[] args)
         {
-
+            //Console.WriteLine($"Llamada al metodo Divide 2 numeros (10 y 5): {Metodos.divideNumeros(10, 5)}");
         }
     }
 }
